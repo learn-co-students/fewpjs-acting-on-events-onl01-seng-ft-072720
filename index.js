221d36a2ef6f3f98028c49c38d1bc4bff309c3e2
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-26 15:43:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+      dodger.style.backgroundColor = "#FF69B4";
+    }
+  }
+
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+      dodger.style.backgroundColor = "#00FF00"; 
+    }
+  }
+
+  function moveDodgerUp() {
+    let upNumbers = dodger.style.bottom.replace("px", "");
+    let up = parseInt(upNumbers, 10);
+   
+    if (up >= 0) {
+      dodger.style.bottom = `${up + 1}px`;
+      dodger.style.backgroundColor = "#FFFFFF"; 
+    }
+  }
+
+
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+  });
