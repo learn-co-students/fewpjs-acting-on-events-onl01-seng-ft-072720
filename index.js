e4bcb04e894da2995e113e8ed85936ff12865622
Your Username <you@example.com>
2020-10-30 15:11:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function preventRefreshOnSubmit(){
+    document.querySelector('form').addEventListener('submit', function(event){
+      event.preventDefault()
+    })
+}
+
+preventRefreshOnSubmit()
